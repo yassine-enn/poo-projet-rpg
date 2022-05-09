995ae58c1b7f2f2e9ecb7348a4e4d1c555563fe2
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-05-09 10:54:55 +0200

    add isplayable flag to see if character is playable or not (monster)

--- christiano.ts
+++ christiano.ts
@@ -1,5 +1,6 @@
 import {Character} from './character.ts';
 class ChristianoPenaldo extends Character{
+    isPlayable = false;
     siuuu(target : Character) : void {
        target.currentHealth -= (this.physicalAttack-target.physicalDefense);
     } 
