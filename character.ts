3b34724ce374e9590502f40ba8ba03dadc23bf88
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-05-09 08:44:58 +0200

    add second attack

--- character.ts
+++ character.ts
@@ -7,6 +7,14 @@ export class Character {
     currentHealth : number = 0;
     maxMana : number = 0;
     currentMana : number = 0;
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number) {
+        this.name = name;
+        this.physicalAttack = physicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.speed = speed;
+        this.maxHealth = maxHealth;
+        this.currentHealth = currentHealth;
+    }
     attack(target : Character) : void {
         target.currentHealth -= (this.physicalAttack-target.physicalDefense);
     }
@@ -14,5 +22,9 @@ export class Character {
         
     }
     revive(target : Character) : void {
+    }
+    attack2(target : Character) : void {
+        console.log("attack2")
+        target.currentHealth -= (this.physicalAttack-target.physicalDefense);
     }
 }
--- thief.ts
+++ thief.ts
@@ -1,5 +1,16 @@
 import {Character} from './character.ts';
 class thief extends Character {
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number) {
+        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth);
+    }
     taxFraud() : void {
+        console.log("fraude fiscale")
+    }
+    attack2() {
+       return this.taxFraud(); 
     }
 }
+
+
+let thief1 = new thief("thief", 10, 10, 50, 100, 100);
+thief1.attack2()
