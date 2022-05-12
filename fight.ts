fd9dbae0b20231d122043892bb56b7b8d3cfdd50
Nathan BOURRY <nathan.bourry@ynov.com>
2022-05-12 16:56:43 +0200

    FixBug: si pas d'item reproposer

--- fight.ts
+++ fight.ts
@@ -1,14 +1,19 @@
 import {Character} from './character.ts';
+import {Menu} from './menu.ts';
+import {Inventory} from './inventory.ts';
+
 export class Fight {
     public teams : [Character[], Character[]] ;
-    public revivable : Character[];
-    public speedOrder : Character[];
+    public revivable : Character[] |null;
+    public speedOrder : Character[] = [];
     public turn : number;
-    constructor(teams : [Character[], Character[]], revivable : Character[],speedOrder : Character[], turn : number) {
+    public fightStatus : string | null;
+    constructor(teams : [Character[], Character[]], revivable : Character[] | null ,speedOrder : Character[] , turn : number, fightStatus : string | null) {
         this.teams = teams;
         this.revivable = revivable;
         this.speedOrder = speedOrder;
         this.turn = turn;
+        this.fightStatus = fightStatus;
     }
     public order() : void {
         for (let i = 0; i < this.teams.length; i++) {
@@ -17,6 +22,40 @@ export class Fight {
             }
             this.speedOrder.sort(function(a,b){return a.speed-b.speed});
             this.speedOrder.reverse();
+        }
 }
+    public startTurn(character : Character, menu:Menu, inventory : Inventory): void {
+        this.order();
+        while (this.fightStatus === null) {
+            menu.chooseAction(character,inventory);
+        for (let i = 0; i < this.speedOrder.length; i++) {
+            if (character.isPlayable){
+                
+            }
+        }
+    }
+    }
+    public fightStatusCheck() : void {
+        if (this.teams[0].length === 0) {
+            this.fightStatus = "victory";
+        } else if (this.teams[1].length === 0) {
+            this.fightStatus = "defeat";
+        }
+    }
 }
-}
+
+
+let character1 = new Character("character1", 10, 10, 50, 100, 100);
+let character2 = new Character("character2", 10, 10, 65, 100, 100);
+let character3 = new Character("character3", 10, 10, 15, 100, 100);
+let character4 = new Character("character4", 10, 10, 25, 100, 100);
+let character5 = new Character("character5", 10, 10, 37, 100, 100);
+let character6 = new Character("character6", 10, 10, 100, 100, 100);
+
+let fight1 = new Fight([[character1, character2, character3], [character4, character5, character6]],null,[], 0, null);
+fight1.order()
+console.log(fight1.speedOrder)
+
+let menu = new Menu([character4, character5, character6]);
+//let inventory = new Inventory(2,3,4,5);
+//sfight1.startTurn(character1, menu, inventory);
--- menu.ts
+++ menu.ts
@@ -1,16 +1,104 @@
 import {Character} from './character.ts';
+import {Inventory} from './inventory.ts';
+import {Fight} from './fight.ts';
 
 export class Menu {
-    chooseAttack(character : Character) : void {
-        let choice = prompt("Choose an attack: ");  
-        switch (choice) {
-            case "1":
-                character.attack1();
+    targetsList : Character[] 
+    constructor(targetsList : Character[] ) {
+        this.targetsList = targetsList;
+    }
+    chooseAttack(character : Character, target : Character) : void {
+        let validChoose : boolean = false
+        while (!validChoose){
+            let choice = prompt("Choose an attack: ");  
+            switch (choice) {
+                case "1":
+                    character.attack(target);
+                    validChoose = true
+                break;
+                case "2":
+                    character.attack2(target);
+                    if (character.currentheal !== character.currentheal - character.attack2) {
+                        console.log("The attack is not possible")
+                    }else {
+                        validChoose = true
+                    }
+                break;
+            }
+        }
+    }
+    useItem(character : Character, item : Inventory) : void {
+        let ChooseItem : boolean = false
+        while (!ChooseItem){
+            let choice = prompt("Choose an item: ");
+            switch (choice) {
+                case "1":
+                    if (item.numberOfPotions > 0) {
+                    item.numberOfPotions --;
+                    character.heal();
+                    ChooseItem = true
+                    } else {
+                        console.log("You don't have any potions")
+                    }
+                break;
+                case "2":  
+                    if (item.numberOfStarFragments > 0) {
+                        item.numberOfStarFragments --;
+                        ChooseItem = true
+                        if (character.currentHealth !== 0){
+                            character.heal();
+                        }else{
+                            character.revive();
+                        }
+                    } else {
+                        console.log("You don't have any star fragments")
+                    }
+                break;
+                case "3":
+                    if (item.numberOfHalfStars > 0) {
+                        item.numberOfHalfStars --;
+                        ChooseItem = true
+                        if (character.currentHealth !== 0){ 
+                            character.heal();
+                        }else{
+                        character.revive();
+                        } 
+                    } else {
+                        console.log("You don't have any half stars")
+                    }
+                break;
+                case "4":
+                    if (item.numberOfEther > 0) {
+                        item.numberOfEther --;
+                        ChooseItem = true
+                        character.currentMana += 30;
+                    } else {
+                        console.log("You don't have any ether")
+                    }
+                break;
+                case "5":
+                    this.chooseAction(character, item)
+                break
+            }
+        }
+    }
+        
+    chooseAction(character : Character, item : Inventory): void {
+        let actionChoice = prompt("Choose an action: ");
+        let i : number = 0
+        let p =  prompt("Choose a target ") 
+        if (p !== null) {
+        i = parseInt(p) - 1;
+        }
+        switch (actionChoice) {
+            case "1": 
+                this.chooseAttack(character,this.targetsList[i]);
                 break;
             case "2":
-                character.attack2();
+                this.useItem(character, item);
                 break;
-            case "3":
-                
+        }
+
+    }
     
 }
