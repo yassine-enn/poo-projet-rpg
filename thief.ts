60a783c1708ac52de30295d449b2af4c8de9cb4d
Mostapha <mostapha@DESKTOP-E3FFDPH.localdomain>
2022-05-12 08:24:28 +0200

    thief.ts

---
+++ thief.ts
@@ -0,0 +1,16 @@
+import {Character} from './character.ts';
+export class Thief extends Character {
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number) {
+        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth,0,0,0);
+    }
+    taxFraud(target: Character) : void {
+        console.log("fraude fiscale")
+    }
+    attack2(target: Character) : void {
+       return this.taxFraud(target); 
+    }
+}
+
+
+let thief1 = new Thief("thief", 10, 10, 50, 100, 100);
+thief1.attack2(thief1)
