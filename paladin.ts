aff6f5493b18028e3b8fbb61dc4f3d87011e5f04
Mostapha <mostapha@DESKTOP-E3FFDPH.localdomain>
2022-05-12 17:19:52 +0200

    paladin.ts

---
+++ paladin.ts
@@ -0,0 +1,12 @@
+import {Character} from './character.ts';
+export class Paladin extends Character {
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number) {
+        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth,0,0,0);
+    }
+    deusVult(target : Character) : void {
+         target.currentHealth -= (this.physicalAttack-target.physicalDefense)*0.4;
+     }
+     attack2(target : Character) : void {
+        return this.deusVult(target); 
+     }
+ }
