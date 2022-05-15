6e85df023135fea9025194c8e9136bf92faa0c42
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-05-15 19:17:01 +0200

    can start a fight loop, only need fix a bug (extra turn after mobs die)

--- fight.ts
+++ fight.ts
@@ -34,11 +34,31 @@ export class Fight {
         }
         console.log(this.speedOrder.length)
 }
-    public startTurn(character : Character, menu:Menu, inventory : Inventory): void {
+// character : Character,
+    public startTurn(menu:Menu, inventory : Inventory): void {
         this.order();
+        for (let i = 0; i < this.speedOrder.length; i++) {
+            console.log(this.speedOrder[i].name);
+        }
+        
         while (this.fightStatus === null) {
-            menu.chooseAction(character,inventory);
-            for (let i = 0; i < this.enemies.length; i++) {
+            // this.fightStatusCheck();
+            // console.log(this.fightStatus);
+            // menu.chooseAction(character,inventory);
+            // for (let i = 0; i < this.enemies.length; i++) {
+            //     if (this.enemies[i].currentHealth <= 0) {
+            //         this.enemies.splice(i,1);
+            //     }
+            // }
+            // for (let i=0; i<this.allies.length; i++) {
+            //     if (this.allies[i].currentHealth <= 0) {
+            //         this.revivable.push(this.allies[i]);
+            //         this.allies.splice(i,1);
+            //     }
+            // }
+            this.fightStatusCheck();
+        for (let i = 0; i < this.speedOrder.length; i++) {
+              for (let i = 0; i < this.enemies.length; i++) {
                 if (this.enemies[i].currentHealth <= 0) {
                     this.enemies.splice(i,1);
                 }
@@ -49,31 +69,68 @@ export class Fight {
                     this.allies.splice(i,1);
                 }
             }
-            console.log(this.speedOrder.length);
             this.fightStatusCheck();
             console.log(this.fightStatus);
-        for (let i = 0; i < this.speedOrder.length; i++) {
-            if (character.isPlayable){
-                
+            if(this.fightStatus === "over") {
+                break;
+            }else{
+                if (this.speedOrder[i].isPlayable && this.speedOrder[i].currentHealth > 0) {
+                    console.log(this.speedOrder[i].name+"\'s turn");
+                    this.fightStatusCheck();
+                    if(this.fightStatus === "over") {
+                        break;
+                    }else{
+                        menu.chooseAction(this.speedOrder[i],inventory);
+                    }
+                }
+                else if(!this.speedOrder[i].isPlayable && this.speedOrder[i].currentHealth > 0) {
+                    console.log(this.speedOrder[i].name+" is playing");
+                    this.fightStatusCheck();
+                    if(this.fightStatus === "over") {
+                        break;
+                        }else{
+
+                            this.speedOrder[i].attack(this.speedOrder[0])
+                        }
+                }
             }
+            
+        }
+    
+        if(this.allies.length === 0) {
+            console.log("defeat !");
+            break;
+        }
+        if(this.enemies.length === 0){
+            console.log("victory !");
+            break;
+        }
         }
     }
-    }
-    public fightStatusCheck() : void {
-        if (this.enemies.length === 0) {
-            this.fightStatus = "victory";
-        } else if (this.allies.length === 0) {
-            this.fightStatus = "defeat";
-        }
+    // public fightStatusCheck() : void {
+    //     if (this.enemies.length === 0) {
+    //         this.fightStatus = "victory";
+    //     } else if (this.allies.length === 0) {
+    //         this.fightStatus = "defeat";
+    //     }
+    // }
+        public fightStatusCheck() : void {
+        if (this.enemies.length === 0 || this.allies.length === 0) {
+            this.fightStatus = "over";
     }
 }
+}
 
-let priestess1 = new Priestess("priestess", 10, 50, 50, 100, 100, 100, 100);
-let warrior1 = new Warrior("warrior", 100, 100, 50, 100, 100);
-let paladin1 = new Paladin("paladin", 100, 80, 50, 100, 100);
-let monster1 = new Monster("monster", 10, 30, 50, 100, 100);
-let monster2 = new Monster("monster", 10, 20, 50, 100, 100);
-let monster3 = new Monster("monster", 10, 20, 50, 100, 100);
+
+
+// let priestess1 = new Priestess("priestess", 10, 50, 50, 20, 100, 100, 100);
+// let warrior1 = new Warrior("warrior", 100, 80, 55, 100, 100);
+let paladin1 = new Paladin("paladin", 100, 80, 60, 100, 80);
+let warrior1 = new Paladin("paladin2", 100, 80, 61, 100, 80);
+let priestess1 = new Paladin("paladin3", 100, 80, 29, 100, 80);
+let monster1 = new Monster("monster", 95, 30, 30, 100, 100);
+let monster2 = new Monster("monster", 95, 30, 31, 100, 100);
+let monster3 = new Monster("monster", 95, 30, 32, 100, 100);
 
 let fight1 = new Fight([[priestess1, paladin1, warrior1], [monster1, monster2, monster3]],[],[], 0, null, [priestess1, paladin1, warrior1], [monster1, monster2, monster3]);
 
@@ -81,4 +138,4 @@ let fight1 = new Fight([[priestess1, paladin1, warrior1], [monster1, monster2, m
 let menu = new Menu(fight1.teams);
 let inventory = new Inventory(2,3,4,5);
 inventory.displayInventory();
-fight1.startTurn(paladin1, menu, inventory);
+fight1.startTurn( menu, inventory);
