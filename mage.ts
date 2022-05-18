4d8201dfe9606f266929a36739fa3bf5f391411c
Mostapha <mostapha@DESKTOP-E3FFDPH.localdomain>
2022-05-18 08:37:10 +0200

    mage.ts

--- mage.ts
+++ mage.ts
@@ -1,12 +1,16 @@
 import {Character} from './character.ts';
 export class Mage extends Character {
     constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number , maxMana : number, currentMana : number, magicAttack : number) {
-        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth, maxMana, currentMana, magicAttack);
+        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth, maxMana, currentMana, magicAttack,"mage");
     }
-    rasengan(target : Character) : void {
-        target.currentHealth -= this.magicAttack;
+    rasengan(targets : Character[]) : void {
+        for (let i = 0; i < targets.length; i++) {
+        targets[i].currentHealth -= this.magicAttack;
+        console.log(this.name + " uses rasengan on " + targets[i].name + " for " + this.magicAttack + " damage");
+        console.log(targets[i].name + " has " + Math.max(targets[i].currentHealth,0) + " health left");
+        }
     }
-    attack2(target : Character) : void {
-        return this.rasengan(target); 
+    attackAOE(targets : Character[]) : void {
+        return this.rasengan(targets); 
      }
 }
