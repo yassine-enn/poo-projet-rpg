46ca571d27155cf6118272ffb5707fd236354e8a
Mostapha <mostapha@DESKTOP-E3FFDPH.localdomain>
2022-05-18 08:33:48 +0200

    chestRoom.ts

--- chestRoom.ts
+++ chestRoom.ts
@@ -1,11 +1,14 @@
 import { Character } from "./character.ts";
+import {Inventory} from './inventory.ts';
 
-class ChestRoom {
+export class ChestRoom {
     chooseCharacter : Character
     item1 : string | null = null;
     item2 : string | null = null;
-    constructor(team : Character[]) {
+    inventory : Inventory
+    constructor(team : Character[], inventory : Inventory) {
         this.chooseCharacter = team[Math.floor(Math.random() * team.length)]
+        this.inventory = inventory
     }
     openchest() {
         if (Math.floor(Math.random() * 2) === 0) {
@@ -16,32 +19,40 @@ class ChestRoom {
             switch (Math.floor(Math.random() * 4)) {
                 case 0:
                     this.item1 = "potion"
+                    this.inventory.numberOfPotions ++
                     break;
                 case 1:
                     this.item1 = "starFragment"
+                    this.inventory.numberOfStarFragments ++
                     break;
                 case 2:
                     this.item1 = "ether"
+                    this.inventory.numberOfEther ++
                     break;
                 case 3:
                     this.item1 = "halfStar"
+                    this.inventory.numberOfHalfStars ++
+                    break;
             }
             switch (Math.floor(Math.random() * 4)) {
                 case 0:
                     this.item2 = "potion"
+                    this.inventory.numberOfPotions ++
                     break;
                 case 1:
                     this.item2 = "starFragment"
+                    this.inventory.numberOfStarFragments ++
                     break;
                 case 2:
                     this.item2 = "ether"
+                    this.inventory.numberOfEther ++
                     break;
                 case 3:
                     this.item2 = "halfStar"
+                    this.inventory.numberOfHalfStars ++
                     break;
             }
             console.log(`You found ${this.item1} and ${this.item2}`)
-            return this.item1, this.item2
         }
     }
 }
