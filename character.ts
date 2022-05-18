4c8021959164396890c36f44ceb153b9764b249e
Mostapha <mostapha@DESKTOP-E3FFDPH.localdomain>
2022-05-18 08:32:35 +0200

    character.ts

--- character.ts
+++ character.ts
@@ -1,4 +1,6 @@
 export class Character {
+    isPlayable : boolean = true;
+    alive : boolean = true;
     name : string = "";
     physicalAttack : number = 0;
     physicalDefense : number = 0;
@@ -8,7 +10,8 @@ export class Character {
     maxMana : number = 0;
     currentMana : number = 0;
     magicAttack : number = 0;
-    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number, maxMana : number, currentMana : number,magicAttack : number) {
+    characterclass : string = "";
+    constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number, maxMana : number, currentMana : number,magicAttack : number, characterclass : string) {
         this.name = name;
         this.physicalAttack = physicalAttack;
         this.physicalDefense = physicalDefense;
@@ -18,15 +21,46 @@ export class Character {
         this.maxMana = maxMana;
         this.currentMana = currentMana;
         this.magicAttack = magicAttack;
+        this.characterclass = characterclass;
     }
     attack(target : Character) : void {
-        target.currentHealth -= (this.physicalAttack-target.physicalDefense);
+        if (target.physicalDefense <= this.physicalAttack) {
+        target.currentHealth -= this.physicalAttack-target.physicalDefense; 
+        }else{
+            target.currentHealth = target.currentHealth;
+        }
+        console.log("physcial attack", this.physicalAttack);
+        console.log("defense: " + target.physicalDefense);
+        console.log("damage: " + (this.physicalAttack-target.physicalDefense));
+        console.log(this.name + " attacks " + target.name + " with " + this.physicalAttack + " damage");
+        console.log(target.name + " has " + Math.max(target.currentHealth,0) + " health left");
+        if (target.currentHealth <= 0) {
+            target.die();
+        }
     }
-    heal(target : Character) : void {
-        
+    heal() : void {
+        console.log("previous health: " + this.currentHealth);
+        if(this.currentHealth + 50 > this.maxHealth) {
+            this.currentHealth = this.maxHealth;
+        }else {
+        this.currentHealth += 50;
+        }
+        console.log(this.name + " heals for 50 health, and now has " + this.currentHealth + " health");
     }
-    revive(target : Character) : void {
+    revive() : void {
+        this.alive = true;
     }
     attack2(target : Character) : void {
+        console.log("attack2")
+        target.currentHealth -= (this.physicalAttack-target.physicalDefense);
     }
+    attackAOE(targets : Character[]) : void {
+    }
+    die() : void {
+        if (this.currentHealth <= 0) {
+            this.alive = false;
+        }
+        console.log(this.name + " is dead");
+    }
+    
 }
