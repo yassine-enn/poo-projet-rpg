e0289d7eb14f9c2d40713230c01cad6548a4908d
Mostapha <mostapha@DESKTOP-E3FFDPH.localdomain>
2022-05-18 08:39:19 +0200

    paladin.ts

--- paladin.ts
+++ paladin.ts
@@ -1,12 +1,15 @@
 import {Character} from './character.ts';
 export class Paladin extends Character {
     constructor(name : string, physicalAttack : number, physicalDefense : number, speed : number, maxHealth : number, currentHealth : number) {
-        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth,0,0,0);
+        super(name, physicalAttack, physicalDefense, speed, maxHealth, currentHealth,0,0,0,"paladin");
     }
-    deusVult(target : Character) : void {
-         target.currentHealth -= (this.physicalAttack-target.physicalDefense)*0.4;
+    deusVult(targets : Character[]) : void {
+        for (let i = 0; i < targets.length; i++) {
+        targets[i].currentHealth -= (this.physicalAttack-targets[i].physicalDefense)*0.4;
+        console.log(this.name + " uses deusVult on " + targets[i].name + " for " +(this.physicalAttack-targets[i].physicalDefense)*0.4  + " damage");
+        console.log(targets[i].name + " has " + Math.max(targets[i].currentHealth,0) + " health left");     }
+    }
+     attackAOE(targets : Character[]) : void {
+        return this.deusVult(targets); 
      }
-     attack2(target : Character) : void {
-        return this.deusVult(target); 
-     }
- }
+}
